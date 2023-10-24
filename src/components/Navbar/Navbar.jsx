9bf8302d3113aec15a173c63f1acc5684bea7343
EmeraldDesignz <amazingemeralddesigns@gmail.com>
2023-10-24 20:32:06 +0530

    Updated Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,14 +4,14 @@ import logo from '../../assets/logo.png';
 
 const Navbar = () => {
   return (
-    <div className="navbar">
+    <div className="navbar w-4/5 mx-auto mt-5">
       <div className="navbar-brand">
         <img src={logo} alt="Company Logo" className="logo" />
       </div>
       <ul className="navbar-nav">
-        <li className="nav-item">Volume</li>
-        <li className="nav-item">Inventory</li>
-        <li className="nav-item">Select Wallet</li>
+        <li className="nav-item"><button>Volume</button></li>
+        <li className="nav-item"><button>Inventory</button></li>
+        <li className="nav-item"><button>Select Wallet</button></li>
       </ul>
     </div>
   );
